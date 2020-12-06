225816f11bdf2b018910b1e198277cf3b23db5d9
Andrii Zakharchuk <andriiz@nvidia.com>
2020-12-06 21:51:16 +0100

    Day 1 part 2 Dat 2 part 1&2

--- Day1/main.py
+++ Day1/main.py
@@ -6,12 +6,12 @@ def read_input_data():
     return content
 
 
-def clean_data(data):
+def clean_data(data, max_number):
     result_data = []
     keys_buffer = {}
     for d in data:
         if d in keys_buffer:
-            if keys_buffer[d] < 2:
+            if keys_buffer[d] < max_number:
                 result_data.append(d)
                 keys_buffer[d] += 1
         else:
@@ -20,24 +20,54 @@ def clean_data(data):
     return result_data
 
 
-data = read_input_data()
-data = clean_data(data)
+def part1():
+    data = read_input_data()
+    data = clean_data(data, 2)
 
-already_checked = {}
+    already_checked = {}
 
-is_result_founded = False
-innerStart = 0
+    is_result_founded = False
 
-for i in range(len(data)):
-    if data[i] in already_checked:
-        continue
-    else:
-        already_checked[data[i]] = 1
+    for i in range(len(data)):
+        if data[i] in already_checked:
+            continue
+        else:
+            already_checked[data[i]] = 1
 
-    for j in range(i+1, len(data)):
-        if (data[i] + data[j]) == 2020:
-            is_result_founded = True
-            print(data[i] * data[j])
+        for j in range(i+1, len(data)):
+            if (data[i] + data[j]) == 2020:
+                is_result_founded = True
+                print(data[i] * data[j])
+                break
+        if is_result_founded:
             break
-    if is_result_founded:
-        break
+
+
+def part2():
+    data = read_input_data()
+    data = clean_data(data, 3)
+
+    already_checked = {}
+
+    is_result_founded = False
+
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            if f"{data[i]},{data[j]}" in already_checked:
+                continue
+            else:
+                already_checked[f"{data[i]},{data[j]}"] = 1
+                already_checked[f"{data[j]},{data[i]}"] = 1
+            for k in range(j + 1, len(data)):
+                if (data[i] + data[j] + data[k]) == 2020:
+                    is_result_founded = True
+                    print(data[i] * data[j] * data[k])
+                    break
+            if is_result_founded:
+                break
+
+
+i = 0
+i += 1
+print("sdfsdf")
+part2()
---
+++ Day2/main.py
@@ -0,0 +1,40 @@
+class Record:
+    def __init__(self, min, max, pattern, password):
+        self.par1: int = min
+        self.par2: int = max
+        self.pattern: str = pattern
+        self.password: str = password
+
+
+def read_input_data():
+    records = []
+    with open('./input') as f:
+        content = f.readlines()
+
+    for line in content:
+        line.strip()
+        words = line.split(" ")
+        min_max = words[0].split("-")
+        records.append(Record(int(min_max[0]), int(min_max[1]), words[1][:-1], words[2]))
+    return records
+
+
+def part1():
+    data = read_input_data()
+    result = 0
+    for d in data:
+        if d.par1 <= d.password.count(d.pattern) <= d.par2:
+            result += 1
+    print(result)
+
+
+def part2():
+    data = read_input_data()
+    result = 0
+    for d in data:
+        if (d.password[d.par1-1] == d.pattern) != (d.password[d.par2 - 1] == d.pattern):
+            result += 1
+    print(result)
+
+
+part2()
